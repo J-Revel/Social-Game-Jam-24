0fdb942506e8b5929a6b39dea2a885763b45c709
J-Revel <j.revel@protonmail.com>
2024-11-30 18:58:41 +0100

    Added configurable shop menu

--- Assets/Scripts/Shop/ShopMenu.cs
+++ Assets/Scripts/Shop/ShopMenu.cs
@@ -2,10 +2,20 @@ using UnityEngine;
 
 public class ShopMenu : MonoBehaviour
 {
+    public ProductConfig[] products;
+    public RectTransform product_container;
+    public RectTransform popup_container;
     public ShopBuyButton buy_button_prefab;
+    public DeckPanel deck_panel;
     void Start()
     {
-        
+        for(int i=0; i<products.Length; i++)
+        {
+            ShopBuyButton button = Instantiate(buy_button_prefab, product_container);
+            button.deckPanel = deck_panel;
+            button.product = products[i];
+            button.popup_container = popup_container;
+        }
     }
 
     void Update()
